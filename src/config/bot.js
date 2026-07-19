cf29d8c1c112b342c58e1eb20d268c96a0b19253
craftyboy9923232-blip <craftyboy9923232@gmail.com>
2026-07-19 00:46:36 -0500

    Update bot status to a Discord invite link

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Helping @TGS",     // this is what people actually see
+        state: "https://discord.gg/JwWhYdhByW",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
